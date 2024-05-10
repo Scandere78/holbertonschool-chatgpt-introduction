e19ba9901e1634afbe0ffde38afc80ebc4162c9e
Scandere78 <scanderetejpro@gmail.com>
2024-05-10 15:24:32 +0200

    add the files

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,13 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1 
+        n = n - 1
     return result
-
-if len(sys.argv) != 2:
-    print("Usage: python3 script.py <number>")
-    sys.exit(1)
 
 f = factorial(int(sys.argv[1]))
 print(f)
-
